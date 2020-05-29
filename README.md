b6bbfe0f9944ca2b1a9d33ea4841039c9dc73b8a
Sebastian Kemi <sebastian.kemi@arbetsformedlingen.se>
2020-05-29 11:37:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### Installation
+
+`npm install team-vagledning/mikrosegmentering`
+
+
+### Användning
+
+```
+const mikro = require('mikrosegmentering)
+
+mikro.getMikrosegmentering({
+    ssyk: 11,
+    alder: 35,
+    bosatt: 1,
+    utbildningsniva: 3
+})
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,84 @@
+const data = require('../data/mikrosegmentering.json')
+
+const classify = (point, centroids) => {
+    var min = Infinity, index = 0;
+ 
+    const distance = (v1, v2) => {
+        var total = 0
+        for (var i = 0; i < v1.length; i++) {
+           total += Math.pow(v2[i] - v1[i], 2)
+        }
+        return Math.sqrt(total)
+     };
+ 
+    for (var i = 0; i < centroids.length; i++) {
+       var dist = distance(point, centroids[i])
+       if (dist < min) {
+          min = dist
+          index = i
+       }
+    }
+ 
+    return index
+ }
+
+const excelHeaderToNumber = (val) => {
+    var base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', i, j, result = 0
+
+    for (i = 0, j = val.length - 1; i < val.length; i += 1, j -= 1) {
+        result += Math.pow(base.length, j) * (base.indexOf(val[i]) + 1)
+    }
+
+    return result
+}
+
+const getFieldValue = (record, excelField) =>
+{
+    let value = record['FIELD' + excelHeaderToNumber(excelField)]
+
+    if (isNaN(value)) {
+        return parseFloat(value.replace(',', '.'))
+    }
+    
+    return value
+}
+
+module.exports.getMikrosegment = (inputs) => {
+
+    // Get selected yrkes
+    const yrkesdata = data.filter(yrke => getFieldValue(yrke, 'A') == inputs.ssyk)
+
+    // Fetch means and deviations, 
+    // there're the same for all clusters of the same yrke
+    const means = {
+        medelalder: getFieldValue(yrkesdata[0], 'DH'),
+        utbildningsniva: getFieldValue(yrkesdata[0], 'DI'),
+        bosatt: getFieldValue(yrkesdata[0], 'DJ')
+    }
+    const deviations = {
+        medelalder: getFieldValue(yrkesdata[0], 'DK'),
+        utbildningsniva: getFieldValue(yrkesdata[0], 'DL'),
+        bosatt: getFieldValue(yrkesdata[0], 'DM')
+    }
+
+    // Make centroids
+    let centroids = []
+    for (const r of yrkesdata) {
+        centroids.push([
+            (getFieldValue(r, 'C') - means.medelalder) / deviations.medelalder,
+            (getFieldValue(r, 'D') - means.utbildningsniva) / deviations.utbildningsniva,
+            (getFieldValue(r, 'E') - means.bosatt) / deviations.bosatt
+        ])
+    }
+
+    // Make input centroid and classify
+    const key = classify([
+        (inputs.alder - means.medelalder) / deviations.medelalder,
+        (inputs.utbildningsniva - means.utbildningsniva) / deviations.utbildningsniva,
+        (inputs.bosatt - means.bosatt) / deviations.bosatt
+    ], centroids)
+
+    return yrkesdata[key]
+}
+
+
