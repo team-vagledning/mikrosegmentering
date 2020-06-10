11576c1be8c4016b02c6f615d4b71a885c36557b
Sebastian Kemi <sebekish@gmail.com>
2020-06-10 09:03:14 +0200

    Get name of yrke and return related yrken

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-//const data = require('../data/mikrosegmentering.json')
 import data from '../data/mikrosegmentering.json'
+import yrken from '../data/yrken.json'
+import likhetsanalys from '../data/likhetsanalys.json'
 
 const classify = (point, centroids) => {
     var min = Infinity, index = 0;
@@ -49,7 +50,35 @@ const getFieldStringValue = (record, excelField) =>
     return record['FIELD' + excelHeaderToNumber(excelField)]
 }
 
-module.exports.getMikrosegment = (inputs) => {
+const getYrke = (ssyk, withRelated = true) => {
+    // Get selected yrke
+    const yrke = yrken.filter(yrke => getFieldNumericValue(yrke, 'A') == ssyk).pop()
+
+    // Get yrkesdata
+    const yrkesdata = data.filter(yrke => getFieldNumericValue(yrke, 'A') == ssyk).pop()
+    
+    // Result set
+    let results = {
+        ssyk: getFieldNumericValue(yrke, 'A'),
+        name: getFieldStringValue(yrke, 'B'),
+        forvantad_automatisering: getFieldNumericValue(yrkesdata, 'DG'),           
+        forvantad_automatisering_klass: getFieldStringValue(yrkesdata, 'DH'),       
+        mobilitetsindex: getFieldNumericValue(yrkesdata, 'DI'),                     
+    }
+
+    // Get related yrken
+    if (withRelated) {
+        const related = likhetsanalys[ssyk].map((r) => {
+            return getYrke(r, false)
+        })
+
+        results['relaterade_yrken'] = related
+    }
+
+    return results
+}
+
+const getMikrosegment = (inputs) => {
 
     // Get selected yrkes
     const yrkesdata = data.filter(yrke => getFieldNumericValue(yrke, 'A') == inputs.ssyk)
@@ -86,7 +115,7 @@ module.exports.getMikrosegment = (inputs) => {
 
     const m = yrkesdata[key]
     
-    return {
+    const yrke = {
         ssyk: getFieldNumericValue(m, 'A'),
         mikrosegment: getFieldNumericValue(m, 'B'),
         medelalder: getFieldNumericValue(m, 'C'),
@@ -94,27 +123,29 @@ module.exports.getMikrosegment = (inputs) => {
         medelandel_bosatt_sverige: getFieldNumericValue(m, 'E'),
         andel_studerande_senaste_aret: getFieldNumericValue(m, 'AU'),
         medelinkomst: getFieldNumericValue(m, 'AW'),
-        forvantad_automatisering: getFieldNumericValue(m, 'DG'),
-        forvantad_automatisering_klass: getFieldStringValue(m, 'DH'),
-        mobilitetsindex: getFieldNumericValue(m, 'DI'),
-        andel_flodat_till_arbetsloshet: getFieldNumericValue(m, 'DP'),
+        andel_flodat_till_arbetsloshet: getFieldNumericValue(m, 'DP'),       
         andel_kvar_i_yrket: getFieldNumericValue(m, 'DQ'),
         andel_bytt_yrke_topp_3: [
-            {
+            Object.assign({
                 ssyk: getFieldNumericValue(m, 'DR'),
                 andel: getFieldNumericValue(m, 'DU')
-            },
-            {
+            }, getYrke(getFieldNumericValue(m, 'DR'), false)),
+
+            Object.assign({
                 ssyk: getFieldNumericValue(m, 'DS'),
                 andel: getFieldNumericValue(m, 'DV')
-            },
-            {
+            }, getYrke(getFieldNumericValue(m, 'DS'), false)),
+
+            Object.assign({
                 ssyk: getFieldNumericValue(m, 'DT'),
                 andel: getFieldNumericValue(m, 'DW')
-            }
+            }, getYrke(getFieldNumericValue(m, 'DT'), false)),
         ],
         andel_bytt_yrke_ovriga: getFieldNumericValue(m, 'DX')
     }
+
+    return Object.assign(yrke, getYrke(inputs.ssyk))
 }
 
-
+module.exports.getYrke = getYrke
+module.exports.getMikrosegment = getMikrosegment
